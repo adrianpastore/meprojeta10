a033bc359b2154787923dc9e36b9aa64acd92954
helenaleivas <lennaleivas@gmail.com>
2018-12-05 21:02:35 -0200

    Add files via upload

---
+++ meprojeta/alterar/empresa.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  require_once('../classes/empresa.php');
+  require_once('../DAO/empresaDAO.php');
+  $empresaDao = new EmpresaDao();
+  $empresa = new Empresa($_POST['nomeEmpresa'],$_POST['cnpj'],$_POST['enderecoEmpresa']);
+  $empresaDao->att($_GET['codcliente'], $cliente->getNome(),$cliente->getCPF(),$cliente->getEmail());
+*/
+  require_once('../navegacao/empresa.php');
+
+?>
